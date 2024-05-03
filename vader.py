acbcd4f9e1af72da59328433c1f62d02e2c0dad7
BenjaminLi <25benjaminli@gmail.com>
2024-05-03 07:14:57 -0400

    more updates, need to stratify select 40 based on each day

--- vader.py
+++ vader.py
@@ -10,7 +10,6 @@ from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
 vader = SentimentIntensityAnalyzer()
 
-
 class HistoricalSentiment:
 
     def __init__(self, ticker, fn=vader.polarity_scores):
@@ -28,6 +27,11 @@ class HistoricalSentiment:
     
     def generate_news_df(self, news_table):
         news_list = []
+        # oldest.datetime_timestamp
+        # datetime.datetime(1998, 11, 11, 18, 45, 51)
+
+        # TODO: filter based on time (i.e. use previous day to get news for next day)
+
         for i in news_table.findAll('tr'):
             try:
                 text = i.a.get_text()
@@ -55,32 +59,49 @@ class HistoricalSentiment:
         news_df = pd.DataFrame(news_list, columns=columns)
         news_df['date'] = pd.to_datetime(news_df.date, format='mixed').dt.date
 
+        # randomly select 40 headlines from 40 different days. This will have to be stratified by date
+        for i in range(40):
+            news_df = news_df.sample(frac=1).groupby('date').head(1)
+
         return news_df
     
     def calculate_sentiment(self):
-        news_table = self.find_articles()
-        self.news_df = self.generate_news_df(news_table)
+        self.news_scraped = self.find_articles()
+        self.news_df = self.generate_news_df(self.news_scraped)
         # requires that find_articles has been called and generated a news_df
 
         scores = self.news_df['headline'].apply(self.fn).tolist()
         scores = [x['compound'] for x in scores]
         sentiment = float(np.mean(scores))
         final_sentiment = round(sentiment, 4)
-        return final_sentiment
+        print(self.news_df.head())
+        return self.news_df['headline'], final_sentiment
     
 # def get_urls():
 with open("finvizurls.txt", "r") as f:
     urls = f.readlines()
 
+random.seed(0)
+# format of each url is https://web.archive.org/web/20210102072011/https://www.finviz.com/
+# need to get the timestamp from each url. Make sure the timestamp is the market close of each day
+# 20210102072011 -> 2021-01-02 07:20:11 UTC
+
 urls_select = random.sample(range(len(urls)), 40)
+urls_select = [urls[i] for i in urls_select]
 news_tables = {}
+
+tickers = ["MSFT"]
+
+print("urls selected", urls_select)
 
 for url in urls_select:
     # save url to access metadata
     # beatiful soup to extract the text
     url = url.strip()
     for ticker in tickers:
-        news_table = HistoricalSentiment(ticker).find_articles()
+        headline, sentiment = HistoricalSentiment(ticker).calculate_sentiment()
+        print("headline", headline)
+        print("sentiment", sentiment)
         break
 
 
@@ -89,44 +110,16 @@ for url in urls_select:
 # time                                                  05:03PM
 # source                                            Barrons.com
 # headline    Section 230 Keeps Coming Up in Congress. Heres...
-news_list = []
-def clean_news_tables():
+# news_list = []
 
-    for file_name, news_table in news_tables.items():
-        for i in news_table.findAll('tr'):
-            try:
-                text = i.a.get_text()
-            except:
-                continue
 
-            date_scrape = i.td.text.split()
-            source = i.div.span.get_text()
+# #TODO: compare vader polarity vs our version
 
-            if len(date_scrape) == 1:
-                time = date_scrape[0]
+# def get_scores(df):
+#     scores = df['headline'].apply(vader.polarity_scores).tolist()
+#     scores = [x['compound'] for x in scores]
+#     sentiment = float(np.mean(scores))
+#     final_sentiment = round(sentiment, 4)
+#     return final_sentiment
 
-            else:
-                final_date = date_scrape[0]
-                time = date_scrape[1]
-
-                if final_date == "Today":
-                    final_date = date.today().strftime("%Y-%m-%d") # b d y??
-
-            tick = file_name.split('_')[0]
-
-            news_list.append([tick, final_date, time, source, text])
-
-    columns = ['ticker', 'date', 'time', 'source', 'headline']
-    news_df = pd.DataFrame(news_list, columns=columns)
-    news_df['date'] = pd.to_datetime(news_df.date, format='mixed').dt.date # format mixed??
-
-#TODO: compare vader polarity vs our version
-
-def get_scores(df):
-    scores = df['headline'].apply(vader.polarity_scores).tolist()
-    scores = [x['compound'] for x in scores]
-    sentiment = float(np.mean(scores))
-    final_sentiment = round(sentiment, 4)
-    return final_sentiment
-
-print("vader", get_scores(news_df))
+# # print("vader", get_scores(news_df))
